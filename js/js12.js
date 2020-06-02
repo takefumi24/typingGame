bdfff94a28bcf458812bd94f9c17bab3d1b6b1b4
takefumi24 <t_ohba24@yahoo.co.jp>
2020-06-02 23:01:00 +0900

    「タイマーの不具合」をコメント化

---
+++ js/js12.js
@@ -0,0 +1,72 @@
+'use strict';
+
+{
+  //タイマーの不具合を修正しよう
+  const words = [
+    'apple',
+    'sky',
+    'blue',
+    'middle',
+    'set',
+  ];
+  let word = words[Math.floor(Math.random() * words.length)];
+  let loc = 0;
+  let score = 0;
+  let miss = 0;
+  const timeLimit = 3 * 1000;
+  let startTime;
+
+  const target = document.getElementById('target');
+  const scoreLabel = document.getElementById('score');
+  const missLabel = document.getElementById('miss');
+  const timerLabel = document.getElementById('timer');
+
+  function updateTarget() {
+    let placeholder = '';
+    for (let i = 0; i < loc; i++) {
+      placeholder += '_';
+    }
+    target.textContent = placeholder + word.substring(loc);
+  }
+
+  function updateTimer() {
+    const timeLeft = startTime + timeLimit - Date.now();
+    timerLabel.textContent = (timeLeft / 1000).toFixed(2);
+
+    const timeoutId = setTimeout(() => {
+      updateTimer();
+    }, 10);
+
+    if (timeLeft < 0) {
+      clearTimeout(timeoutId);
+      // タイムアウトした時のアラートを0.00と表示する
+      timerLabel.textContent = '0.00';
+      //アラートの処理を少し遅らせる
+      setTimeout(() => {
+        alert('Game Over');
+      },100)
+    }
+  }
+
+  window.addEventListener('click', () => {
+    target.textContent = word;
+    startTime = Date.now();
+    updateTimer();
+  });
+
+  window.addEventListener('keydown', e => {
+    if (e.key === word[loc]) {
+      loc++;
+      if (loc === word.length) {
+        word = words[Math.floor(Math.random() * words.length)];
+        loc = 0;
+      }
+      updateTarget();
+      score++;
+      scoreLabel.textContent = score;
+    } else {
+      miss++;
+      missLabel.textContent = miss;
+    }
+  });
+}
