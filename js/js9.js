ff62c3dd349320047de295dce486e424d81a5a67
takefumi24 <t_ohba24@yahoo.co.jp>
2020-06-02 18:06:19 +0900

    タイマー処理のコメントと言語化

---
+++ js/js9.js
@@ -0,0 +1,47 @@
+'use strict';
+//ゲームをスタートさせよう
+{
+  const words = [
+    'apple',
+    'sky',
+    'blue',
+    'middle',
+    'set',
+  ];
+  let word = words[Math.floor(Math.random() * words.length)];
+  let loc = 0;
+  let score = 0;
+  let miss = 0;
+
+  const target = document.getElementById('target');
+  const scoreLabel = document.getElementById('score');
+  const missLabel = document.getElementById('miss');
+
+
+  function updateTarget() {
+    let placeholder = '';
+    for (let i = 0; i < loc; i++) {
+      placeholder += '_';
+    }
+    target.textContent = placeholder + word.substring(loc);
+  }
+  //初期画面のclick to startを押したときの処理
+  window.addEventListener('click', () => {
+    target.textContent = word;
+  });
+  window.addEventListener('keydown', e => {
+    if (e.key === word[loc]) {
+      loc++;
+      if (loc === word.length) {
+        word = words[Math.floor(Math.random() * words.length)];
+        loc = 0;
+      }
+      updateTarget();
+      score++;
+      scoreLabel.textContent = score;
+    } else {
+      miss++;
+      missLabel.textContent = miss;
+    }
+  });
+}
